d27a4094d9590c4e7b5e1fb29ec27dd05cdecf56
João Pedro Turatti Souza <71042838+aopedro@users.noreply.github.com>
2023-09-03 13:38:08 -0300

    src.py

--- src.py
+++ src.py
@@ -1,4 +1,5 @@
 array = [] #our array
+doInfo = open("info.txt", "a")
 
 def decrement(a): #decrement sort algorithm
     for i in range(1, len(a)):
@@ -7,10 +8,21 @@ def decrement(a): #decrement sort algorithm
             a[j - 1], a[j] = a[j], a[j - 1]
             j -= 1
 
+def book(a):
+    doInfo.write('early results:\n')
+    for j in range(0, len(a)):
+        doInfo.write(str(array[j]) + ', ')
+
+    doInfo.write('\nAdd Descriptions: \n')
+
+def book_desc(string):
+        book(array)
+        doInfo.write(string)
+
 def function():
-    num1 = input("Number 1: ") #get num1
-    num2 = input("Number 2: ") #get num2
-    division = int(num2)/int(num1) #make the division
+    num1 = input("Favorable cases: ") #get num1
+    num2 = input("Total cases: ") #get num2
+    division = int(num1)/int(num2) #make the division
     percent = division*100 #turn results of division in percent's
     array.extend([int(percent)]) #put the result in the end of the array
     again = input("Continue? (y/n): ") 
@@ -21,9 +33,11 @@ def function():
         #decrement 
         decrement(array)
         print(array)
-        
-    else:
-        print("Getting out...")
-        return 0
+        print("\n")
+        desc = input("ADD DESCRIPTION: ")
+        book_desc(desc)
     
+    
+
 function()
+input("Press any key to exit...")
